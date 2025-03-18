93f8035f75b71af97cb57100c43cb3e25ef4748f
DeltaVictor2021300 <134743051+DeltaVictor2021300@users.noreply.github.com>
2025-03-18 16:09:34 -0400

    Add files via upload
    Initial commit

---
+++ piOptic.py
@@ -0,0 +1,116 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+Be sure to check the learn guides for more usage information.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+
+Author(s): Melissa LeBlanc-Williams for Adafruit Industries
+"""
+
+import digitalio
+import board
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw
+from adafruit_rgb_display import ili9341
+from adafruit_rgb_display import st7789  # pylint: disable=unused-import
+from adafruit_rgb_display import hx8357  # pylint: disable=unused-import
+from adafruit_rgb_display import st7735  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
+from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=172, height=320, x_offset=34, # 1.47" ST7789
+# disp = st7789.ST7789(spi, rotation=270, width=170, height=320, x_offset=35, # 1.9" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True, width=80,       # 0.96" MiniTFT Rev A ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, invert=True, width=80,    # 0.96" MiniTFT Rev B ST7735R
+# x_offset=26, y_offset=1,
+disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+#disp = ili9341.ILI9341(
+#    spi,
+#    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image)
+
+
+# Open video capture
+cap = cv2.VideoCapture(0)  # /dev/video0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+        
+    height, width = frame.shape[:2]
+    frame = frame[:height // 2, :]
+	
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (128, 128))
+
+    # Convert resized frame to PIL Image
+    frame_pil = Image.fromarray(frame_resized)
+    draw = ImageDraw.Draw(frame_pil)
+    width, height = frame_pil.size
+    center_x, center_y = width // 2, height // 2
+    draw.line([(center_x, 0), (center_x, height)], fill="red", width=2)
+    draw.line([(0, center_y), (width, center_y)], fill="red", width=2)
+    
+    radius = 5
+    draw.ellipse(
+		(center_x - radius, center_y - radius, center_x + radius, center_y + radius),
+		fill="red"
+	)
+	
+    # Send frame to OLED
+    disp.image(frame_pil)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
